37fc6c8564af2d2f2fcdfa26baf8aaa8fd1e0271
FranckNk <nkeuboufranck@gmail.com>
2022-03-20 23:53:52 -0300

    Finalisation du code, fonctionnement des LED, remplacement des fonctions "digitalWrite" par "analogWrite" afin de palier au problème de modification du signal après un analogWrite, non-fix des LED qui s'allument au début du code.

--- src/main.cpp
+++ src/main.cpp
@@ -26,68 +26,82 @@ Keypad keypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
 // Type pour gérer les états du programme, afin d'éviter les bugs.
 enum MODE{
-	DEPART, 					// On lance le programme. Aucune situation... L'appui des touches ne servira à rien
+	TERMINE, 					// On lance le programme. Aucune situation... L'appui des touches ne servira à rien
 	SELECTION_LED,				// On doit choisir la LED à utiliser.
 	SELECTION_INTENSITE,		// On doit choisir son intensité.
-	TERMINE,					// Toute les réglages sont faites, on a terminé. On peut maintenant régler la LED.
+					// Toute les réglages sont faites, on a terminé. On peut maintenant régler la LED.
 };
 
 // Déclaration de la structure LED qui contient la lettre et la PIN pour l'affichage dans le moniteur série.
 struct LED
 {
-	char Lettre;
-	short int Broche;
+	char Lettre = 'A'; // La LED A est sélectionnée par défaut.
+	uint8_t Broche;
+	uint8_t Intensite = 255; // Valeur par défaut de l'intensité de la LED.
 };
 
 LED MesLED[4];
 
-//initialize an instance of class NewKeypad
-byte ledPin = 12; 
-
-boolean blink = false;
-boolean ledPin_state;
 
 // Déclaration des constantes.
-const int LED_A = 0;
-const int LED_B = 1;
-const int LED_C = 2;
-const int LED_D = 10;
+const int LED_A = 2;
+const int LED_B = 3;
+const int LED_C = 4;
+const int LED_D = 5;
 
 // Déclaration des variables.
-bool ChoixLED = false;
-bool ChoixIntensite = false;
-
-short int GetIntensite = 0;
+short int IndexLEDUtilise = 3;
 MODE Situation;
 
 void keypadEvent(KeypadEvent key);
+void ChooseLED(char LEDValue);
+void ConfigureeLED();
+void RestaureIntensiteLEDs();
 
 void setup(){
 		Serial.begin(9600);
-		pinMode(ledPin, OUTPUT);              // Sets the digital pin as output.
-		digitalWrite(ledPin, HIGH);           // Turn the LED on.
-		ledPin_state = digitalRead(ledPin);   // Store initial LED state. HIGH when LED is on.
+		pinMode(LED_A, OUTPUT);              // Sets the digital pin as output.
+		pinMode(LED_B, OUTPUT);              // Sets the digital pin as output.
+		pinMode(LED_C, OUTPUT);              // Sets the digital pin as output.
+		pinMode(LED_D, OUTPUT);              // Sets the digital pin as output.
+		ConfigureeLED();
 		keypad.addEventListener(keypadEvent); // Add an event listener for this keypad
-		Situation = DEPART; 					// Car on vient de démarrer le programme. On peut alors choisir une LED.
+		Situation = TERMINE; 					// Car on vient de démarrer le programme. On peut alors choisir une LED.
 }
 
 void loop(){
 		char keys = keypad.getKey();
 
 		if (keys) {
-				Serial.println(keys);
-		}
-		if (Situation == DEPART){
-
-		}
-		if (Situation == TERMINE)
-		{
-			
+			Serial.println(keys);
+		Serial.print("La valeur du mode est : ");
+		Serial.println(Situation);
 		}
 		
-		if (blink){
-				digitalWrite(ledPin,!digitalRead(ledPin));    // Change the ledPin from Hi2Lo or Lo2Hi.
-				delay(100);
+		if (keys && Situation == SELECTION_LED && keys >= 'A' && keys <= 'D'){
+			ChooseLED(keys); // On allume la LED correspondante.
+		}
+		if (keys && Situation == SELECTION_INTENSITE && keys <= '9' && keys >= '0')
+		{
+			short int ValeurIntensite = int (keys) - 48;
+			Serial.print("La valeur de l'intensité est : ");
+			
+			MesLED[IndexLEDUtilise].Intensite = map(ValeurIntensite, 0, 9, 0, 255);
+			Serial.println(MesLED[IndexLEDUtilise].Intensite);
+			Serial.print("La LED choisie est : ");
+			Serial.println(MesLED[IndexLEDUtilise].Lettre);
+			// Appliquons l'intensité à temps réel de sélection sur la LED choisie.
+			analogWrite(MesLED[IndexLEDUtilise].Broche, ValeurIntensite);
+		}
+		
+		if (Situation == TERMINE){
+			
+			Serial.print("La valeur de l'index est : ");
+			Serial.println(IndexLEDUtilise);
+			analogWrite(MesLED[IndexLEDUtilise].Broche, MesLED[IndexLEDUtilise].Intensite);    // Change the Broche from Hi2Lo or Lo2Hi.
+			delay(100);
+			analogWrite(MesLED[IndexLEDUtilise].Broche, 0);    // Change the Broche from Hi2Lo or Lo2Hi.
+			delay(100);
 		}
 }
 
@@ -100,26 +114,25 @@ void keypadEvent(KeypadEvent key){
 				break;
 
 		case RELEASED:
-				if (key == '*') {
-						digitalWrite(ledPin,ledPin_state);    // Restore LED state from before it started blinking.
-						blink = false;
-				}
+				
 				break;
 
 		case HOLD:	
-				if (key == '0' && Situation == DEPART)
+				if (key == '0' && Situation == TERMINE)
 				{
 					Situation = SELECTION_LED; // On passe alors au mode suivant.
+					// Extension de toutes les LED pour choisir la LED.
+					analogWrite(LED_A, 0);
+					analogWrite(LED_B, 0);
+					analogWrite(LED_C, 0);
+					analogWrite(LED_D, 0);
 				}
 							
-				if (key == '#' && Situation == DEPART) {
-						ChoixLED = true;
+				if (key == '#' && Situation == SELECTION_LED) {
 						Situation = SELECTION_INTENSITE; // On peut maintenant choisir l'intensité de la LED.
 				}
 				if (key == '*' && Situation == SELECTION_INTENSITE) {
-						ChoixIntensite = true;    	// Blink the LED when holding the * key.
 						Situation = TERMINE; 		// On passe à l'état Terminé pour régler la LED.
-						ChoixLED = false;
 
 				}
 				break;
@@ -130,31 +143,35 @@ void ChooseLED(char LEDValue){
 	switch (LEDValue)
 	{
 	case 'A':
-		digitalWrite(LED_A, HIGH);
-		digitalWrite(LED_B, LOW);
-		digitalWrite(LED_C, LOW);
-		digitalWrite(LED_D, LOW);
+		analogWrite(LED_A, 255);
+		analogWrite(LED_B, 0);
+		analogWrite(LED_C, 0);
+		analogWrite(LED_D, 0);
+		IndexLEDUtilise = 0;
 		break;
 	
 	case 'B':
-		digitalWrite(LED_A, LOW);
-		digitalWrite(LED_B, HIGH);
-		digitalWrite(LED_C, LOW);
-		digitalWrite(LED_D, LOW);
+		analogWrite(LED_A, 0);
+		analogWrite(LED_B, 255);
+		analogWrite(LED_C, 0);
+		analogWrite(LED_D, 0);
+		IndexLEDUtilise = 1;
 		break;
 	
 	case 'C':
-		digitalWrite(LED_A, LOW);
-		digitalWrite(LED_B, LOW);
-		digitalWrite(LED_C, HIGH);
-		digitalWrite(LED_D, LOW);
+		analogWrite(LED_A, 0);
+		analogWrite(LED_B, 0);
+		analogWrite(LED_C, 255);
+		analogWrite(LED_D, 0);
+		IndexLEDUtilise = 2;
 		break;
 	
 	case 'D':
-		digitalWrite(LED_A, LOW);
-		digitalWrite(LED_B, LOW);
-		digitalWrite(LED_C, LOW);
-		digitalWrite(LED_D, HIGH);
+		analogWrite(LED_A, 0);
+		analogWrite(LED_B, 0);
+		analogWrite(LED_C, 0);
+		analogWrite(LED_D, 255);
+		IndexLEDUtilise = 3;
 		break;
 	
 	default:
@@ -170,5 +187,13 @@ void ConfigureeLED(){
 	MesLED[2].Broche = LED_C;
 	MesLED[2].Lettre = 'C';
 	MesLED[3].Broche = LED_D;
-	MesLED[3].Lettre = 'D';
+	MesLED[3].Lettre = 'D';	
+}
+
+void RestaureIntensiteLEDs(){
+	for (int i = 0; i < 0; i++)
+	{
+		MesLED[i].Intensite = 9;
+	}
+	
 }
